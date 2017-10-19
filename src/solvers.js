b6ae65a119b455b81ccba02e21790527a5f14f05
Hack Reactor Student <student@hackreactor.com>
2017-10-19 12:14:34 -0700

    Complete queens functions, all solvers

--- src/solvers.js
+++ src/solvers.js
@@ -13,10 +13,67 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
+window.copy = function(board) {
+  var copy = [];
+  board.rows().forEach(row =>
+    copy.push(row.slice()));
+  return copy;
+};
 
 window.findNRooksSolution = function(n) {
  
+  var board = new Board(makeEmptyMatrix(n));
+  
+  var solutions = [];  
+
+  // create inner recursive function
+  var solve = function(piecesPlaced, row) {
+    if (solutions.length > 0) {
+      return solutions[0];
+    }
+    var piecesPlaced = piecesPlaced; 
+    // iterate through columns
+    for (let col = 0; col < n; col++) {
+      // set piece in the square
+      board.togglePiece(row, col);
+      // increment pieces placed
+      piecesPlaced++;
+      // check if we have a valid board
+      if (!board.hasAnyRooksConflicts()) {
+        // if we've placed n pieces we have a solution 
+        if (piecesPlaced === n) {
+          // add it to our solution list
+          // solutions.push(JSON.parse(JSON.stringify(board.rows())));
+          solutions.push(copy(board));   
+        } else {
+          // we're not done trying to turn this board into a solution
+          // recurse if there are more rows below
+          if (row < n - 1) {
+            solve(piecesPlaced, row + 1);          
+          }
+        }
+      }
+      // remove piece so we can look for more solutions 
+      board.togglePiece(row, col);
+      // decrement pieces placed
+      piecesPlaced--;
+    }
+    if (row < n - 1) {
+      solve(piecesPlaced, row + 1);
+    }
+  };
+  // start at row 0 with 0 pieces placed
+  solve(0, 0);
+
+  //console.log(solutions);
+  
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions[0]));
+  return solutions[0];
+};
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = function(n) {
+  
   var board = new Board(makeEmptyMatrix(n));
   
   var solutions = [];  
@@ -35,7 +92,9 @@ window.findNRooksSolution = function(n) {
         // if we've placed n pieces we have a solution 
         if (piecesPlaced === n) {
           // add it to our solution list
-          solutions.push(JSON.parse(JSON.stringify(board.rows()))); 
+          // solutions.push(JSON.parse(JSON.stringify(board.rows())));
+          solutions.push(copy(board));   
+ 
         } else {
           // we're not done trying to turn this board into a solution
           // recurse if there are more rows below
@@ -53,20 +112,8 @@ window.findNRooksSolution = function(n) {
       solve(piecesPlaced, row + 1);
     }
   };
-  // what if we recursed through each square?
-  // maybe too much, just exploring
   // start at row 0 with 0 pieces placed
   solve(0, 0);
-
-  //console.log(solutions);
-  
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions[0]));
-  return solutions[0];
-};
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-
  
 
   console.log('Number of solutions for ' + n + ' rooks:', solutions.length);
@@ -75,16 +122,114 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var board = new Board(makeEmptyMatrix(n));
+  
+  var solutions = [];  
+
+  // create inner recursive function
+  var solve = function(piecesPlaced, row) {
+    if (solutions.length > 0) {
+      return solutions[0];
+    }    
+    var piecesPlaced = piecesPlaced; 
+    // iterate through columns
+    for (let col = 0; col < n; col++) {
+      // set piece in the square
+      board.togglePiece(row, col);
+      // increment pieces placed
+      piecesPlaced++;
+      // check if we have a valid board
+      if (!board.hasAnyQueensConflicts()) {
+        // if we've placed n pieces we have a solution 
+        if (piecesPlaced === n) {
+          // add it to our solution list
+          // solutions.push(JSON.parse(JSON.stringify(board.rows())));
+          solutions.push(copy(board));   
+ 
+        } else {
+          // we're not done trying to turn this board into a solution
+          // recurse if there are more rows below
+          if (row < n - 1) {
+            solve(piecesPlaced, row + 1);          
+          }
+        }
+      }
+      // remove piece so we can look for more solutions 
+      board.togglePiece(row, col);
+      // decrement pieces placed
+      piecesPlaced--;
+    }
+    if (row < n - 1) {
+      solve(piecesPlaced, row + 1);
+    }
+  };
+  // start at row 0 with 0 pieces placed
+  solve(0, 0);
+  
+  if (n < 1) {
+    return [];
+  }
+
+  if (!solutions.length) {
+    solutions[0] = board.rows();
+  }
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solutions[0]));
+  return solutions[0];
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  
+  var board = new Board(makeEmptyMatrix(n));
+  
+  var solutions = [];  
 
+  // create inner recursive function
+  var solve = function(piecesPlaced, row) {
+    var piecesPlaced = piecesPlaced; 
+    // iterate through columns
+    for (let col = 0; col < n; col++) {
+      // set piece in the square
+      board.togglePiece(row, col);
+      // increment pieces placed
+      piecesPlaced++;
+      // check if we have a valid board
+      if (!board.hasAnyQueensConflicts()) {
+        // if we've placed n pieces we have a solution 
+        if (piecesPlaced === n) {
+          // add it to our solution list
+          // solutions.push(JSON.parse(JSON.stringify(board.rows())));
+          solutions.push(copy(board));   
+ 
+        } else {
+          // we're not done trying to turn this board into a solution
+          // recurse if there are more rows below
+          if (row < n - 1) {
+            solve(piecesPlaced, row + 1);          
+          }
+        }
+      }
+      // remove piece so we can look for more solutions 
+      board.togglePiece(row, col);
+      // decrement pieces placed
+      piecesPlaced--;
+    }
+    if (row < n - 1) {
+      solve(piecesPlaced, row + 1);
+    }
+  };
+  // start at row 0 with 0 pieces placed
+  solve(0, 0);
+  
+  var solutionCount;
+  if (n < 1) {
+    return 1;
+  } else {
+    solutionCount = solutions.length;
+  }
+  
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
